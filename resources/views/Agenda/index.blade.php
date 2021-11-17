fbff3c1fd093d6a0da5dfe744e306f55d56da13e
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-17 04:47:14 -0600

    Inicio del proyecto

---
+++ resources/views/Agenda/index.blade.php
@@ -0,0 +1,277 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                  <div class="row">
+            <div id="calendar" class="col-xl-8 col-md-6 col-sm-12 "></div>
+                 <div class="col-xl-4 col-md-6 col-sm-12 bg-dark text-white">
+                    <!-- <div id="calendarioWeb2"></div> -->
+                   
+                    <div class="col-12">
+                        <div class="card text-center">
+                            <h5 class="card-title text-dark mt-10">INFO PACIENTES</h5>
+                          <div class="card-header">
+                            <ul class="nav nav-tabs card-header-tabs">
+                              <li class="nav-item">
+                                <a class="nav-link active" href="#">Informacion</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Historial Medico</a>
+                              </li>
+                               <li class="nav-item">
+                                <a class="nav-link" href="#">Tareas del Medico</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Confirmar Cita</a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div class="card-body bg-dark">
+                             <div class="col-12">
+                                <div class="form-group">
+                            <label for="formGroupExampleInput" >Paciente</label>
+                            <input type="text" class="form-control bg-light" id="formGroupExampleInput" placeholder="Paciente">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Primera cita</label>
+                            <input type="text" value="NO"class="form-control bg-light" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Segunda cita</label>
+                            <input type="text" value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Tercera cita</label>
+                            <input type="text"  value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Cuarta cita</label>
+                            <input type="text" value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                          </div>
+                        </div>
+
+                        <!-- SEGUNDA COLUMNA EN LA PARTE DE LA AGENDA -->
+                            <div class="col-12">
+                                <div class="form-group">
+                            <label for="formGroupExampleInput" >Aseguradora</label>
+                            <input type="text" class="form-control bg-light" id="formGroupExampleInput" placeholder="Primera cita">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Medico</label>
+                            <input type="text" value="NO"class="form-control bg-light" id="formGroupExampleInput2" placeholder="Medico">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Empresa</label>
+                            <input type="text" value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Empresa">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Referido</label>
+                            <input type="text"  value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Referido">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2" >Consultorio</label>
+                            <input type="text" value="NO" class="form-control bg-light" id="formGroupExampleInput2" placeholder="Consultorio">
+                          </div>
+                        </div>
+
+
+
+
+                          </div>
+                        </div>
+                    </div>
+
+                    
+            </div> 
+            <!-- fin del DIV PRINCIPAL DEL FORM -->
+  
+    </div>
+            </div>
+        </div>
+    </div>
+  
+    
+
+       
+
+   
+
+</div>
+
+<script>
+    $(document).ready(function() {
+        console.log('INICIO DE LA AGENDA');
+        $.ajax({
+                  url: "https://api.promo.byw-si.com.mx/api/agenda",
+                  // url: "http://127.0.0.1:8000/api/agenda",
+                  method: "GET",
+                  dataType: "json"
+                }).done(function( data ) {
+                    // alert("Todo bien");
+                    console.log(data,"JSON");
+                    $.each(data, function(i, item) {
+
+                            // console.log(data[i].paciente);
+                          //   $("medicos").each(function(i){ 
+                    //       this.src = "test" + i + ".jpg"; 
+                                // });
+                                // $("#medicos").append('<option value='+data[i].ID+'>'+data[i].nombre+'</option>');
+                        }); //Si pones el content-type en PHP no necesitas parse         
+                });
+
+
+                  
+
+                    var calendarEl = document.getElementById('calendar');
+                    var calendar = new FullCalendar.Calendar(calendarEl, {
+                      initialView: 'dayGridMonth',
+                      locale: 'es',
+                      aspectRatio: 1.5,
+                      headerToolbar: {
+                     start: 'title', // will normally be on the left. if RTL, will be on the right
+                      center: 'dayGridWeek,dayGridMonth,dayGridDay',
+                      end: 'today prev,next' // will normally be on the right. if RTL, will be on the left     
+                 },
+                      // left:'today,prev,next',
+                      //       center:'title',
+                      //       right:'month,basicWeek,basicDay,agendaWeek,agendaDay'
+
+             
+
+
+
+
+                    });
+                    calendar.render();
+
+
+}); //FIN DE DOCUMENT.READY
+</script>
+
+
+<script>
+    //CON ESTA FUNCION SE ACTUALIZA CADA 5 MINUTOS LA PAGINA
+      // setInterval("actualizar()",50000);
+
+    
+    function enviar(){
+                     paciente = $.trim($('#modalPaciente').val());
+                     // alert(paciente);
+                     hora = $.trim($('#hora').val());
+                     edad = $.trim($('#edad').val());
+                     telefono1=$.trim($('#telefono1').val());
+                     telefono2=$.trim($('#telefono2').val());
+                     telefono3=$.trim($('#telefono3').val());
+                     medico=$('#modalMedico').val();
+                     consultorio=$('#modalConsultorio').val();
+                     costo=$('#costoConsulta').val();
+                     recado=$('#recado').val();
+                     fecha= $('#FECHA').val();
+                     data = {
+                          paciente: paciente, 
+                          edad: edad,
+                          hora:hora,
+                          telefono1:telefono1,
+                          telefono2:telefono2,
+                          telefono3:telefono3,
+                          medico:medico,
+                          consultorio:consultorio,
+                          recado:recado,
+                          costo:costo,
+                          fecha:fecha
+                        };
+                    $.ajax({
+                            method:'POST',
+                            // url: 'https://api.promo.byw-si.com.mx/api/agenda',
+                            url: 'http://127.0.0.1:8000/api/agenda',
+                            data: {
+                                action:'cita',
+                                data: data,
+                            }
+                        }).done(item => {
+                            if(item.error){
+                                msg = 'No se encontro al usuario. Verifica tus datos.';
+                                err.append(msg);
+                                err.addClass('alert-danger');
+                               
+                               
+                                btn.before(err);
+                                btn.removeAttr('disabled');
+
+                            }else{
+                                 $("#agendarModal").removeClass('selected');
+                               console.log('Success');
+                                // window.location = "https://promomedics.byw-si.com.mx/calendario.php";
+                            }
+                        }, 'json');
+                        // location.reload();
+
+                }
+                function actualizarCita(){
+                        paciente = $.trim($('#modalPaciente').val());
+                     // alert(paciente);
+                     id_cita = $.trim($('#ID_EDIT').val());
+                     hora = $.trim($('#hora').val());
+                     EDAD = $.trim($('#EDAD_EDIT').val());
+                     TELEFONO1=$.trim($('#telefono1').val());
+                     TELEFONO2=$.trim($('#telefono2').val());
+                     TELEFONO3=$.trim($('#telefono3').val());
+                     medico=$('#modalMedico').val();
+                     consultorio=$('#modalConsultorio').val();
+                     COSTO=$('#costoConsulta_EDIT').val();
+                     RECADO=$('#recado_EDIT').val();
+                     fecha= $('#FECHA').val();
+                     data = {
+                        _token:'{{ csrf_token() }}',
+                          paciente: paciente, 
+                          marco:"hola",
+                          edad: EDAD,
+                          hora:hora,
+                          telefono1:TELEFONO1,
+                          telefono2:TELEFONO2,
+                          telefono3:TELEFONO3,
+                          medico:medico,
+                          consultorio:consultorio,
+                          recado:RECADO,
+                          costo:COSTO,
+                          fecha:fecha,
+                          id_cita:id_cita
+                        };
+                    $.ajax({
+                            method:'POST',
+                            // url: 'https://api.promo.byw-si.com.mx/api/agenda/'+id_cita,
+                            url: 'http://127.0.0.1:8000/api/agenda/'+id_cita,
+                            // type: "PATCH",
+                            data: {
+                                action:'cita',
+                                data: data,
+                            }
+                        }).done(item => {
+                            if(item.error){
+                                msg = 'No se encontro al usuario. Verifica tus datos.';
+                                err.append(msg);
+                                err.addClass('alert-danger');
+                               
+                               
+                                btn.before(err);
+                                btn.removeAttr('disabled');
+
+                            }else{
+                                 $("#agendarModal").removeClass('selected');
+                               console.log('Success');
+                                // window.location = "https://promomedics.byw-si.com.mx/calendario.php";
+                            }
+                        }, 'json');
+                        // location.reload();
+
+                }
+
+      function actualizar(){
+        // location.reload();
+      }
+</script>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,206 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- CALENDARIO -->
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.css">
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/locales-all.js"></script>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.css' rel='stylesheet' />
+        s<link href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css' rel='stylesheet'>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <!-- {{ config('app.name', 'CRM PROMOMEDICS') }} -->
+              <img src="{{ asset('img/logo.png') }}" alt="" class="h-25" style="width: 120px; ">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                     @auth
+                    <ul class="navbar-nav mr-auto">
+                                
+
+                                <!-- SECCION DE LAS PRECARGAS -->
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   {{ __('Precargas') }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Escolaridad') }}
+                                    </a>
+                                      <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Departamentos') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Puestos') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Areas') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Beneficios') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Convenios') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Medios de contacto') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Aseguradoras') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Redes Sociales') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Servicios') }}
+                                    </a>
+
+                                </div>
+                                </li>
+                                <!-- SECCION DE SEGURIDAD -->
+                                    <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   {{ __('Seguridad') }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Modulos') }}
+                                    </a>
+                                      <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Perfiles') }}
+                                    </a>
+                                     <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Usuarios') }}
+                                    </a>
+                                  
+
+                                </div>
+
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Medicos') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Grupo Medico') }}</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Hospitales') }}</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Pacientes') }}</a>
+                                </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('Agenda') }}">{{ __('Agenda') }}</a>
+                                </li>
+                             
+                                
+                                  
+                                
+                    </ul>
+                     @else 
+                     <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                    <!-- <a class="nav-link" href="{{ route('register') }}">{{ __('') }}</a> -->
+                                </li> 
+                     </ul>
+                      @endauth
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Cerrar sesion') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
